1df38c477004c7b02e32e970529f8df8bfb6a6e6
p4cm4n972 <manuel.adele@gmail.com>
2017-01-02 21:46:03 +0100

    ajout dossier image ajout bkg event onclick OK

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function bkg () {
+document.getElementById('bkg').style.backgroundImage = "url('img/bkg.jpg')";
+document.getElementById('canvas').style.backgroundImage = "url('img/font.jpg')";
+document.getElementById('canvas').style.borderColor = "purple";
+
+};
+
+
+var tableauCouleur = ['Aqua','Black', 'Blue', 'Fuchsia', 'Gray', 'Green', 'Lime', 'Maroon', 'Navy', 'Olive', 'Purple', 'Red', 'Silver', 'Teal', 'White', 'Yellow']

c10bb62f848505819b4da36fd3f3155b08063d88
p4cm4n972 <manuel.adele@gmail.com>
2017-02-08 17:01:06 +0100

    mise en forme

--- javascript.js
+++ javascript.js
@@ -1,99 +1,81 @@
-window.addEventListener('load', function () {
+window.addEventListener('load', function () { //CHARGEMENT (LOAD) DE LA PAGE 
 
-    var mur = new Image('width: 600, height: 620');
-    mur.src = 'img/font.jpg';
-    var tableauCouleur = ['Aqua', 'Black', 'Blue', 'Fuchsia', 'Gray', 'Green', 'Lime', 'Maroon', 'Navy', 'Olive', 'Purple', 'Red', 'Silver', 'Teal', 'White', 'Yellow'];
-    var x = 100;
-    var y = 10;
-    var vx = 5;
-    var vy = 2;
-    var q = 170;
-    var s = 580;
-    var u = 430;
-    var v = 580;
+    var context = document.getElementById('canvas').getContext('2d'); //DECLARATION DU CONTEXT DU CANVAS : 2D (2 DIMENSIONS)
+    var mur = new Image('width: 600, height: 620');// CREATION DUN NOUVEL OBJET IMAGE 'MUR' QUI CONTIENDRA L'IMAGE  REPRESENTANT L'IMAGE DE FOND DU CANVAS
+    mur.src = 'img/font.jpg';//ATTRIBUTION DE LA SOURCE DE L'IMAGE 
+    //var tableauCouleur = ['Aqua', 'Black', 'Blue', 'Fuchsia', 'Gray', 'Green', 'Lime', 'Maroon', 'Navy', 'Olive', 'Purple', 'Red', 'Silver', 'Teal', 'White', 'Yellow'];
+//DECALRATION VARIABLES GLOBALES
+    
 
-
-
-
+//DECLARATION DE L'OBJET QUI REPRESENTERA LA BALLE
+    var ball = {
+        x: 100,
+        y: 10,
+        vx: 5,
+        vy: 2,
+        radius: 25,
+        color: 'blue',
+        draw: function () {
+            context.beginPath();
+            context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+            context.closePath();
+            context.fillStyle = this.color;
+            context.fill();
+        }
+    };
+    
+//DECLARATION DE L'OBJET QUI REPRESENTERA LES OBSTACLES ROND
+    var pieces = {
+        x: 500,
+        y: 500,
+        radius: 25,
+        color: 'blue',
+        draw: function () {
+            context.beginPath();
+            context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+            context.closePath();
+            context.fillStyle = this.color;
+            context.fill();
+        }
+    };
+   
+    
+    
+//L'ANIMATION
     function game() {
+        context.drawImage(mur, 0, 0);//DESSIN DU FOND DU CANVAS
+        ball.draw();//DESSIN DE LA BALLE
+        pieces.draw();//DESSIN DES OBSTACLES ROND
 
-        var context = document.getElementById('canvas').getContext('2d');
+       
+        //ALGORITHME DU MOUVEMENT DE LA BALLE
+        ball.x += ball.vx;
+        ball.y += ball.vy;
+        ball.vy *= 0.99;
+        ball.vy += 0.25;
+        //
+        var dx = (ball.x ) - (pieces.x);
+        var dy = (ball.y ) - (pieces.y);
+        var distance = Math.sqrt(dx * dx + dy * dy);
 
-        context.drawImage(mur, 0, 0);
-
-        context.beginPath();
-        context.rect(q, s, 100, 15);
-        context.closePath();
-        context.fillStyle = 'yellow';
-        context.fill();
-        
-        context.beginPath();
-        context.rect(u, v, -100, 15);
-        context.closePath();
-        context.fillStyle = 'yellow';
-        context.fill();
-
-        context.beginPath();
-        context.arc(x, y, 40 / 2, 0, Math.PI * 2);
-        context.closePath();
-        context.fillStyle = 'blue';
-        context.fill();
-        x += vx;
-        y += vy;
-        vy *= 0.99;
-        vy += 0.25;
-
-        if (y + vy > canvas.height || y + vy < 0) {
-            vy = -vy;
+        if (distance < ball.radius + pieces.radius) {
+            ball.vy = -ball.vy;
         }
-        if (x + vx > canvas.width || x + vx < 0) {
-            vx = -vx;
+        //
+       
+
+        if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
+            ball.vy = -ball.vy;
+        }
+        if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
+            ball.vx = -ball.vx;
         }
 
-        if (q < x + 15 && q + 100 > x && 580 < y + 15 && 15 + 580 > y) {
-            vy *= -1.2;
-        }
-        if (u < x + 15 && u + 100 > x && 580 < y + 15 && 15 + 580 > y) {
-            vy *= -1.2;
-        }
+      
 
-        window.onkeydown = function (event) {
-            var code = event.keyCode;
-            switch (code) {
-                case 37:
-                    context.rotate(Math.PI / 2);
-                    break;
-                case 39:
-                    q += 12;
-                    break;
-
-            }
-        }
 
         window.requestAnimationFrame(game);
     };
 
-
-    window.addEventListener('click', function () {
-        document.body.style.margin = '20px';
-        document.getElementById('canvas').style.borderColor = "purple";
-        var laDivDemonBouton = document.createElement('div');
-        laDivDemonBouton.id = 'maDiv';
-        laDivDemonBouton.style.position = 'absolute';
-        laDivDemonBouton.style.margin = 'auto';
-        laDivDemonBouton.style.display = 'block';
-        var monBouton = document.createElement('button');
-        monBouton.id = 'monBouton';
-        monBouton.style.backgroundColor = 'purple';
-        laDivDemonBouton.appendChild(monBouton);
-        document.body.appendChild(laDivDemonBouton);
-        document.getElementById('monBouton').innerHTML = 'play';
-        document.getElementById('monBouton').addEventListener('click', function () {
-            laDivDemonBouton.style.display = 'none';
-
-            return game();
-        })
-    })
-
+    game();
 })//ONLOAD
-
